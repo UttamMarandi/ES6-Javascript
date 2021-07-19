4822d33d8d084caf254577a5fa45916682b3fc02
Uttam Marandi <uttammarandi007@gmail.com>
2021-07-19 10:46:35 +0530

    initial start

---
+++ ES6 Javascript Arrow Functions/index.js
@@ -0,0 +1,10 @@
+//ES6 (fat) Arrow functions
+//works best in callback function scenarios
+//parenthesis around input if more than one
+//curly braces around function body if more than one line command
+//return only needed if more than one line of code
+
+//abbreviated syntax for functoin...no additional functionality
+
+let numbers = [123,234,345,456,567]
+let names = ["uttam" , "tommy", "paras", "salman", "sanjay", "tarun"]
