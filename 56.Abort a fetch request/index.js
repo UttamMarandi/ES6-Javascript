89e323137f6e589be05a2a16ae7d66be44e826c2
Uttam Marandi <uttammarandi007@gmail.com>
2021-08-10 23:29:43 +0530

    Abort a fetch request

--- 56.Abort a fetch request/index.js
+++ 56.Abort a fetch request/index.js
@@ -2,39 +2,3 @@
 //Sometimes when the fetch is taking long , or the user has changed it's mind we want to stop the fecth from happening
 //To abort fetch() we use AbortControllers
 
-const url = `https://jsonplaceholder.typicode.com/posts`;
-
-//click 
-
-document.addEventListener("DOMContentLoaded", ()=>{
-    document.getElementById("btnStart").addEventListener("click", getData)
-})
-
-//AbortControllers
-const fetchController = new AbortController()
-//create a new fetchController
-
-//fetch
-function getData() {
-    
-    const {signal} = fetchController //use of destructuring ..I have no idea
-    //we need to pass {signal} to fetch so that we can use abort() within fetch
-    let lag = setTimeout(()=> {
-        fetchController.abort() //call fetchController.abort() after 5s
-        console.log("aborted after 1s");
-    },1000)
-
-
-    fetch(url , {signal})
-    .then(response => response.json())
-    .then(data => {
-        clearTimeout(lag) //if we get the data we don't want the .abort() method to run
-        //so use clearTimeout
-        console.log("we get the data from json placeholded",data);
-    })
-    .catch(err => {
-        console.warn({err});
-    })
-}
-
-
